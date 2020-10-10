9e485f21f04c059b29df1557475bc00b25dcc140
Dakshin R <drathan45@gmail.com>
2020-10-10 12:12:26 -0400

    Updated Router to have Switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 import logo from "./logo.svg";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
-import ArticlesList from "./pages/ArticlesList";
+import ArticlesListPage from "./pages/ArticlesListPage";
 import NavBar from "./NavBar";
 import "./App.css";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <NavBar/>
+        <NavBar />
         <div id="page-body">
-          <Route path="/" component={Homepage} exact />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/articles-list" component={ArticlesList} />
-          <Route path="/article/:name" component={ArticlePage} />
+          <Switch>
+            <Route path="/" component={Homepage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesListPage} />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
