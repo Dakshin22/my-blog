f0b1f8f315949d423ddab9922f431805dc3ab204
Dakshin R <drathan45@gmail.com>
2020-10-10 12:12:56 -0400

    Add other articles at bottom of Articles

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import articles from "./article-content";
+import ArticlesList from "../components/ArticlesList";
+import NotFoundPage from "./NotFoundPage";
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articles.find((article) => article.name === name);
-  console.log(match);
-
+  const otherArticles = articles.filter(article => article.name !== name);
   if (!article) {
-    return <h1>Article does not exist</h1>;
+    return <NotFoundPage/>;
   }
   return (
     <>
@@ -14,6 +15,8 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
     </>
   );
 };
