a520dea2763ed6a1b7f1393035d0c2dacfe2088a
Dakshin R <drathan45@gmail.com>
2020-10-10 10:53:11 -0400

    Pages and Router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
 import "./App.css";
 
 function App() {
@@ -9,6 +12,9 @@ function App() {
     <Router>
       <div className="App">
         <Route path="/" component={Homepage} exact />
+        <Route path = "/about" component = {AboutPage}/>
+        <Route path = "/articles-list" component = {ArticlesList}/>
+        <Route path = "/article1" component = {ArticlePage}/>
       </div>
     </Router>
   );
---
+++ frontend/src/pages/AboutPage.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const AboutPage = () => (
+  <>
+    <h1>About Me</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Aliquet bibendum enim
+      facilisis gravida neque convallis a cras semper. Etiam tempor orci eu
+      lobortis elementum nibh tellus molestie. Nec ullamcorper sit amet risus
+    </p>
+
+    <p>
+      Nec dui nunc mattis enim ut. Risus viverra adipiscing at in tellus integer
+      feugiat scelerisque varius. Purus sit amet luctus venenatis lectus magna
+      fringilla urna. Facilisis leo vel fringilla est ullamcorper. Dictum at
+      tempor commodo ullamcorper a. Habitant morbi tristique senectus et netus
+    </p>
+
+    <p>
+      Sed tempus urna et pharetra pharetra massa massa ultricies mi. Egestas
+      integer eget aliquet nibh praesent tristique magna. Purus sit amet
+      volutpat consequat mauris nunc congue. Gravida rutrum quisque non tellus
+      orci ac. Nulla facilisi etiam dignissim diam quis. Sed augue lacus viverra
+    </p>
+
+    <p>
+      Nec feugiat nisl pretium fusce id velit. Egestas tellus rutrum tellus
+      pellentesque eu tincidunt tortor. In egestas erat imperdiet sed euismod
+      nisi porta lorem. Urna et pharetra pharetra massa. Bibendum enim facilisis
+      gravida neque convallis a cras semper. Nibh cras pulvinar mattis nunc sed
+      blandit libero volutpat. Nulla malesuada pellentesque elit eget gravida
+      cum sociis natoque. Faucibus turpis in eu mi bibendum neque egestas congue
+      quisque. In metus vulputate eu scelerisque felis imperdiet. Rhoncus urna
+    </p>
+
+    <p>
+      Mus mauris vitae ultricies leo integer malesuada. Massa enim nec dui nunc.
+      Neque aliquam vestibulum morbi blandit cursus risus at ultrices mi.
+      Condimentum id venenatis a condimentum vitae sapien pellentesque habitant
+      morbi. In egestas erat imperdiet sed euismod. Mauris commodo quis
+    </p>
+  </>
+);
+
+export default AboutPage
