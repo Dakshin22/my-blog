d40fbbd760e6aaee08df94418817cb14a93ba4f1
Dakshin R <drathan45@gmail.com>
2020-10-10 11:28:56 -0400

    Map URL parameters and article content

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,9 +1,21 @@
 import React from "react";
+import articles from "./article-content";
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  const article = articles.find((article) => article.name === name);
+  console.log(match);
 
-const ArticlePage = () => (
-  <>
-    <h1>This is an article</h1>
-  </>
-);
+  if (!article) {
+    return <h1>Article does not exist</h1>;
+  }
+  return (
+    <>
+      <h1>This is the {article.title} article</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </>
+  );
+};
 
-export default ArticlePage
+export default ArticlePage;
