2cfe35800a7fd4affc454c809caafb9eda31dd06
Dakshin R <drathan45@gmail.com>
2020-10-10 11:28:31 -0400

    Add url parameters to router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,20 @@ import Homepage from "./pages/Homepage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesList from "./pages/ArticlesList";
+import NavBar from "./NavBar";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={Homepage} exact />
-        <Route path = "/about" component = {AboutPage}/>
-        <Route path = "/articles-list" component = {ArticlesList}/>
-        <Route path = "/article1" component = {ArticlePage}/>
+        <NavBar/>
+        <div id="page-body">
+          <Route path="/" component={Homepage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article/:name" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
